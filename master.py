8cc4ffbd9309da611940d2ac153818a6f23a5e01
mariusmaur <>
2024-01-04 15:48:59 +0100

    removed compleasm from local and updated master.py to exclude the compleasm option

--- master.py
+++ master.py
@@ -3,7 +3,6 @@ import os
 
 # Function to get SRA prefetch data
 def fetch_sra_data(table_path):
-
     # Read the table and get SRA accessions
     with open(table_path, 'r') as infile:
         for line in infile:
@@ -16,24 +15,22 @@ def fetch_sra_data(table_path):
             os.system("cd SRA_prefetch && module load SRA-Toolkit/3.0.3-gompi-2022a && prefetch " +  SRA_ACCESSION)
 
 # Function to create the yaml for running Snakefile_SRA
-def create_sra_yaml(table_path, compleasm):
+def create_sra_yaml(table_path):
     # Convert the table path to its absolute path
     absolute_table_path = os.path.abspath(table_path)
 
     data = f"""samples_table: {absolute_table_path}
-compleasm: {compleasm}
 """
     output_path = os.path.join("yaml", "sra.yaml")
     with open(output_path, 'w') as outfile:
         outfile.write(data)
 
 # Function to create the yaml for running Snakefile_local
-def create_local_yaml(table_path, compleasm):
+def create_local_yaml(table_path):
     # Convert the table path to its absolute path
     absolute_table_path = os.path.abspath(table_path)
 
     data = f"""samples_table: {absolute_table_path}
-compleasm: {compleasm}
 """
     output_path = os.path.join("yaml", "local.yaml")
     with open(output_path, 'w') as outfile:
@@ -44,7 +41,6 @@ def main():
     parser = argparse.ArgumentParser(description="Generate YAML files.")
     
     parser.add_argument('--account', required=True, help='Input slurm account')
-    parser.add_argument('--compleasm', choices=['insecta', 'arachnida', 'endopterygota', 'hymenoptera', 'sauropsida'], help='Dataset')
     parser.add_argument('--trimmomatic', 
                         default="ILLUMINACLIP:/cluster/software/Trimmomatic/0.39-Java-11/adapters/TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:4:30 MINLEN:80", 
                         help='Options for trimmomatic. Default: "ILLUMINACLIP:/cluster/software/Trimmomatic/0.39-Java-11/adapters/TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:4:30 MINLEN:80"')
@@ -68,50 +64,30 @@ def main():
     if not os.path.exists("SRA_prefetch"):
         os.mkdir("SRA_prefetch")
 
-    # Run Snakemake with the --sra option and the local Snakefile
+    # Run Snakemake with the --sra option
     if args.sra:
-        print("")
-        print("---------------------")
-        print("| Fetching SRA data |")
-        print("---------------------")
-        print("")
+        print("\n---------------------\n| Fetching SRA data |\n---------------------\n")
         fetch_sra_data(args.sra)
-        print("")
-        print("-----------------------------------------------")
-        print("| SRA fetch done --> moving to yaml creation! |")
-        print("-----------------------------------------------")
-        print("")
-        create_sra_yaml(args.sra, args.compleasm)
-        print("")
-        print("-----------------------------------------------")
-        print("| sra.yaml has been created in yaml directory |")
-        print("|     Snakemake pipeline started with SRA     |")
-        print("-----------------------------------------------")
-        print("")
+        print("\n-----------------------------------------------\n| SRA fetch done --> moving to yaml creation! |\n-----------------------------------------------\n")
+        create_sra_yaml(args.sra)
+        print("\n-----------------------------------------------\n| sra.yaml has been created in yaml directory |\n|     Snakemake pipeline started with SRA     |\n-----------------------------------------------\n")
         os.system(f"""snakemake --snakefile snakefiles/Snakefile_SRA --keep-going --configfile yaml/sra.yaml \
             --config trimmomatic_options='{args.trimmomatic}' \
             --cluster 'sbatch --account={args.account} \
             --output=0_slurm_logs/{{params.rule_name}}_{{wildcards.sample}}_slurm_%j.log \
             --error=0_slurm_logs/{{params.rule_name}}_{{wildcards.sample}}_slurm_%j.log \
             --time={{resources.time}} --mem={{resources.mem}}MB --cpus-per-task={{resources.cpu}}' --jobs 9999""")
-    # Run Snakemake with the --local option and the local Snakefile
+
+    # Run Snakemake with the --local option
     elif args.local:
-        create_local_yaml(args.local, args.compleasm)
-        print("")
-        print("-------------------------------------------------")
-        print("| local.yaml has been created in yaml directory |")
-        print("|  Snakemake pipeline started with local data   |")
-        print("-------------------------------------------------")
-        print("")
+        create_local_yaml(args.local)
+        print("\n-------------------------------------------------\n| local.yaml has been created in yaml directory |\n|  Snakemake pipeline started with local data   |\n-------------------------------------------------\n")
         os.system(f"""snakemake --snakefile snakefiles/Snakefile_local --configfile yaml/local.yaml \
             --config trimmomatic_options='{args.trimmomatic}' \
             --cluster 'sbatch --account={args.account} \
             --output=0_slurm_logs/{{params.rule_name}}_{{wildcards.sample}}_slurm_%j.log \
             --error=0_slurm_logs/{{params.rule_name}}_{{wildcards.sample}}_slurm_%j.log \
             --time={{resources.time}} --mem={{resources.mem}}MB --cpus-per-task={{resources.cpu}}' --jobs 9999""")
-
-
-
 
 if __name__ == "__main__":
     main()
